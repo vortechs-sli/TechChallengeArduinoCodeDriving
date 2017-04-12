ae4c32b5d26ac43e7fc7fb52e7f8ae4f68b89425
vortechs-sli <vortechs.sli@gmail.com>
2017-04-12 13:32:58 -0700

    Update TechChallengeArduinoCodeDriving.ino

--- TechChallengeArduinoCodeDriving.ino
+++ TechChallengeArduinoCodeDriving.ino
@@ -1,34 +1,33 @@
 
 /**
+    REFERENCES:
     BasicBot - The basic working bot.
-
     Part of the Asimi project - http://sudarmuthu.com/arduino/asimi
-
-   Copyright 2011  Sudar Muthu  (email : sudar@sudarmuthu.com)
-   
+    Copyright 2011  Sudar Muthu  (email : sudar@sudarmuthu.com)
    **/
 
-#define E1 5  // Enable Pin for motor 1
-#define E2 4  // Enable Pin for motor 2
 
-#define I1 3  // Control pin 1 for motor 1
-#define I2 2  // Control pin 2 for motor 1
-#define I3 1  // Control pin 1 for motor 2
-#define I4 0  // Control pin 2 for motor 2
+int E1 = 5;  // Enable Pin for motor 1
+int E2 = 4  // Enable Pin for motor 2
+
+int I1 = 3  // Control pin 1 for motor 1
+int I2 = 2  // Control pin 2 for motor 1
+int I3 = 1  // Control pin 1 for motor 2
+int I4 = 0  // Control pin 2 for motor 2
 
 void setup() {
 
     pinMode(E1, OUTPUT);
     pinMode(E2, OUTPUT);
-//
+
     pinMode(I1, OUTPUT);
     pinMode(I2, OUTPUT);
     pinMode(I3, OUTPUT);
     pinMode(I4, OUTPUT);
-//
+
     digitalWrite(E1, HIGH);
     digitalWrite(E2, HIGH);
-//
+
     digitalWrite(I1, HIGH);
     digitalWrite(I2, LOW);
     digitalWrite(I3, HIGH);
